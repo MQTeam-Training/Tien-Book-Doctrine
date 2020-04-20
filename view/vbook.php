51f373ff023a0cca7ab8a848739ae0cf90adca2c
viettien1992 <viettien321992@gmail.com>
2020-04-20 18:01:32 +0700

    pÃush bÃÃook 3

--- view/vbook.php
+++ view/vbook.php
@@ -21,27 +21,27 @@ $Books = $bookRepository->findAll();
         <?php
 
         foreach ($Books as $Book) {
-            echo sprintf("-%s\n", $Book->getId());
-            echo '
-							<tr>
-							    <td>
-							    <div class="form-check">
-                                  <label class="form-check-label">
-                                    <input type="checkbox" name="myCheckbox[]" class="form-check-input" value='. $Book->getId() .'>
-                                  </label>
-                                </div>
-                                </td>
-						        <td>' . $Book->getId(). '</td>
-				                <td>' . $Book->getSach() . '</td>
-				                <td>' . $Book->getTacGia(). '</td>
-				                <td>' . $Book->getNXB() . '</td>
-				                <td class="text-center">
-				                <a class="btn btn-info btn-xs" href="edit.php?id_Edit=' . $Book->getId() . '">
-				                <span class="glyphicon glyphicon-edit"></span> Edit</a> 
-				                <a href="delete.php?id=' . $Book->getId() . '" class="btn btn-danger btn-xs">
-				                <span class="glyphicon glyphicon-remove"></span> Del</a>
-				                </td> 
-			                </tr>';
+            echo sprintf("-%s\n", $Book->getTacGia());
+//            echo '
+//							<tr>
+//							    <td>
+//							    <div class="form-check">
+//                                  <label class="form-check-label">
+//                                    <input type="checkbox" name="myCheckbox[]" class="form-check-input" value='. $Book->getId() .'>
+//                                  </label>
+//                                </div>
+//                                </td>
+//						        <td>' . $Book->getId(). '</td>
+//				                <td>' . $Book->getSach() . '</td>
+//				                <td>' . $Book->getTacGia(). '</td>
+//				                <td>' . $Book->getNXB() . '</td>
+//				                <td class="text-center">
+//				                <a class="btn btn-info btn-xs" href="edit.php?id_Edit=' . $Book->getId() . '">
+//				                <span class="glyphicon glyphicon-edit"></span> Edit</a>
+//				                <a href="delete.php?id=' . $Book->getId() . '" class="btn btn-danger btn-xs">
+//				                <span class="glyphicon glyphicon-remove"></span> Del</a>
+//				                </td>
+//			                </tr>';
         }
         ?>
     </table>
